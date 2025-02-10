78b4a841ec8e6d9f4d0398081c5fcfb0c9204549
code33dev <alberto.huerta.luna@gmail.com>
2025-02-09 20:19:38 -0600

    partial fix without colors

--- main.cpp
+++ main.cpp
@@ -68,225 +68,141 @@ private:
     QVector<HighlightingRule> rules;
 };
 
-class TextEditor : public QMainWindow
-{
+
+class TextEditor : public QMainWindow {
 
 public:
-    TextEditor()
-    {
-        setWindowTitle("Qt6 Syntax Highlighter with Full Menus");
-        resize(800, 600);
+    TextEditor() {
+        setWindowTitle("Qt6 Text Editor with Tabs & File Explorer");
+        resize(1000, 600);
 
+        // File Explorer
         explorer = new QTreeView(this);
         fileModel = new QFileSystemModel(this);
-        fileModel->setRootPath(""); // Start with no directory loaded
+        fileModel->setRootPath("");
         explorer->setModel(fileModel);
-        explorer->setColumnHidden(1, true); // Hide file size
-        explorer->setColumnHidden(2, true); // Hide file type
-        explorer->setColumnHidden(3, true); // Hide date modified
+        explorer->setColumnHidden(1, true);
+        explorer->setColumnHidden(2, true);
+        explorer->setColumnHidden(3, true);
 
-        editor = new QPlainTextEdit(this);
-        setCentralWidget(editor);
-        highlighter = new SyntaxHighlighter(editor->document());
+        // Tabbed Editor
+        tabWidget = new QTabWidget(this);
+        tabWidget->setTabsClosable(true);
+        tabWidget->setMovable(true);
+        connect(tabWidget, &QTabWidget::tabCloseRequested, this, &TextEditor::closeTab);
 
+        // Splitter for Explorer & Tabs
         QSplitter *splitter = new QSplitter(this);
         splitter->addWidget(explorer);
-        splitter->addWidget(editor);
+        splitter->addWidget(tabWidget);
         splitter->setStretchFactor(1, 1);
         setCentralWidget(splitter);
 
         createMenuBar();
+        connect(explorer, &QTreeView::doubleClicked, this, &TextEditor::openFileFromExplorer);
     }
 
 private:
-    QPlainTextEdit *editor;
-    SyntaxHighlighter *highlighter;
-    QString currentFilePath;
     QTreeView *explorer;
     QFileSystemModel *fileModel;
-    void createMenuBar()
-    {
+    QTabWidget *tabWidget;
+    QMap<QString, QWidget *> openTabs;
+
+    void createMenuBar() {
         QMenuBar *menuBar = new QMenuBar(this);
 
         // File Menu
         QMenu *fileMenu = menuBar->addMenu("&File");
-        fileMenu->addAction("New", QKeySequence::New, this, &TextEditor::newFile);
-        fileMenu->addAction("Open", QKeySequence::Open, this, &TextEditor::openFile);
-        fileMenu->addAction("Save", QKeySequence::Save, this, &TextEditor::saveFile);
-        fileMenu->addAction("Save All", this, &TextEditor::saveAllFiles);
-        fileMenu->addAction("Reload", this, &TextEditor::reloadFile);
-        fileMenu->addAction("Close File", this, &TextEditor::closeFile);
+        fileMenu->addAction("Open Folder", this, &TextEditor::openFolder);
+        fileMenu->addAction("Open File", QKeySequence::Open, this, &TextEditor::openFile);
+        fileMenu->addAction("Save", QKeySequence::Save, this, &TextEditor::saveCurrentFile);
         fileMenu->addSeparator();
         fileMenu->addAction("Exit", QKeySequence::Quit, qApp, &QApplication::quit);
 
         // Edit Menu
         QMenu *editMenu = menuBar->addMenu("&Edit");
-        editMenu->addAction("Select All", QKeySequence::SelectAll, editor, &QPlainTextEdit::selectAll);
-        editMenu->addAction("Copy", QKeySequence::Copy, editor, &QPlainTextEdit::copy);
-        editMenu->addAction("Cut", QKeySequence::Cut, editor, &QPlainTextEdit::cut);
-        editMenu->addAction("Paste", QKeySequence::Paste, editor, &QPlainTextEdit::paste);
+        editMenu->addAction("Select All", QKeySequence::SelectAll, this, &TextEditor::selectAll);
+        editMenu->addAction("Copy", QKeySequence::Copy, this, &TextEditor::copy);
+        editMenu->addAction("Cut", QKeySequence::Cut, this, &TextEditor::cut);
+        editMenu->addAction("Paste", QKeySequence::Paste, this, &TextEditor::paste);
         editMenu->addSeparator();
-        editMenu->addAction("Undo", QKeySequence::Undo, editor, &QPlainTextEdit::undo);
-        editMenu->addAction("Redo", QKeySequence::Redo, editor, &QPlainTextEdit::redo);
+        editMenu->addAction("Undo", QKeySequence::Undo, this, &TextEditor::undo);
+        editMenu->addAction("Redo", QKeySequence::Redo, this, &TextEditor::redo);
         editMenu->addSeparator();
         editMenu->addAction("Find", QKeySequence::Find, this, &TextEditor::findText);
         editMenu->addAction("Replace", QKeySequence::Replace, this, &TextEditor::replaceText);
-        editMenu->addAction("Find in Files", QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_F), this, &TextEditor::findInFiles);
-        editMenu->addAction("Replace in Files", QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_H), this, &TextEditor::replaceInFiles);
-        editMenu->addAction("Go to line", QKeySequence(Qt::CTRL | Qt::Key_F12), this, &TextEditor::goToLine);
-
-        QMenu *settingsMenu = menuBar->addMenu("&Settings");
-        settingsMenu->addAction("Select All", QKeySequence::SelectAll, editor, &QPlainTextEdit::selectAll);
-        settingsMenu->addAction("Copy", QKeySequence::Copy, editor, &QPlainTextEdit::copy);
-        settingsMenu->addAction("Cut", QKeySequence::Cut, editor, &QPlainTextEdit::cut);
-        settingsMenu->addAction("Paste", QKeySequence::Paste, editor, &QPlainTextEdit::paste);
-        settingsMenu->addSeparator();
-        settingsMenu->addAction("Undo", QKeySequence::Undo, editor, &QPlainTextEdit::undo);
-        settingsMenu->addAction("Redo", QKeySequence::Redo, editor, &QPlainTextEdit::redo);
-        settingsMenu->addSeparator();
-        settingsMenu->addAction("Find", QKeySequence::Find, this, &TextEditor::findText);
-        settingsMenu->addAction("Replace", QKeySequence::Replace, this, &TextEditor::replaceText);
-        settingsMenu->addAction("Find in Files", QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_F), this, &TextEditor::findInFiles);
-        settingsMenu->addAction("Replace in Files", QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_H), this, &TextEditor::replaceInFiles);
-        settingsMenu->addAction("Go to line", QKeySequence(Qt::CTRL | Qt::Key_F12), this, &TextEditor::goToLine);
-
-        QMenu *systemDTEngineMenu = menuBar->addMenu("&DTEngine");
-        systemDTEngineMenu->addAction("Select All", QKeySequence::SelectAll, editor, &QPlainTextEdit::selectAll);
-        systemDTEngineMenu->addAction("Copy", QKeySequence::Copy, editor, &QPlainTextEdit::copy);
-        systemDTEngineMenu->addAction("Cut", QKeySequence::Cut, editor, &QPlainTextEdit::cut);
-        systemDTEngineMenu->addAction("Paste", QKeySequence::Paste, editor, &QPlainTextEdit::paste);
-        systemDTEngineMenu->addSeparator();
-        systemDTEngineMenu->addAction("Undo", QKeySequence::Undo, editor, &QPlainTextEdit::undo);
-        systemDTEngineMenu->addAction("Redo", QKeySequence::Redo, editor, &QPlainTextEdit::redo);
-        systemDTEngineMenu->addSeparator();
-        systemDTEngineMenu->addAction("Find", QKeySequence::Find, this, &TextEditor::findText);
-        systemDTEngineMenu->addAction("Replace", QKeySequence::Replace, this, &TextEditor::replaceText);
-        systemDTEngineMenu->addAction("Find in Files", QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_F), this, &TextEditor::findInFiles);
-        systemDTEngineMenu->addAction("Replace in Files", QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_H), this, &TextEditor::replaceInFiles);
-        systemDTEngineMenu->addAction("Go to line", QKeySequence(Qt::CTRL | Qt::Key_F12), this, &TextEditor::goToLine);
+        editMenu->addSeparator();
+        editMenu->addAction("Go to Line", QKeySequence(Qt::CTRL | Qt::Key_F12), this, &TextEditor::goToLine);
 
         setMenuBar(menuBar);
-
-        connect(explorer, &QTreeView::doubleClicked, this, &TextEditor::openFileFromExplorer);
-
     }
 
-    void newFile()
-    {
-        editor->clear();
-        currentFilePath.clear();
-        setWindowTitle("New File - Qt6 Syntax Highlighter");
-    }
-
-    void openFile()
-    {
-        QString filePath = QFileDialog::getOpenFileName(this, "Open File", "", "C++ Files (*.cpp *.h);;All Files (*)");
-        if (filePath.isEmpty())
-            return;
-
-        QFile file(filePath);
-        if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
-        {
-            QMessageBox::warning(this, "Error", "Could not open file");
-            return;
-        }
-
-        QTextStream in(&file);
-        editor->setPlainText(in.readAll());
-        file.close();
-
-        currentFilePath = filePath;
-        setWindowTitle(filePath);
-    }
-    void openFolder()
-    {
+    void openFolder() {
         QString dir = QFileDialog::getExistingDirectory(this, "Select Folder");
-        if (!dir.isEmpty())
-        {
+        if (!dir.isEmpty()) {
             fileModel->setRootPath(dir);
             explorer->setRootIndex(fileModel->index(dir));
         }
     }
-     void openFileFromExplorer(const QModelIndex &index) {
+
+    void openFile() {
+        QString filePath = QFileDialog::getOpenFileName(this, "Open File", "", "Text Files (*.txt *.cpp *.h);;All Files (*)");
+        if (!filePath.isEmpty()) {
+            openFileInTab(filePath);
+        }
+    }
+
+    void openFileFromExplorer(const QModelIndex &index) {
         QString filePath = fileModel->filePath(index);
         if (QFileInfo(filePath).isFile()) {
-            loadFile(filePath);
-        }
-    }
-     void loadFile(const QString &filePath) {
-        QFile file(filePath);
-        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-            QTextStream in(&file);
-            editor->setPlainText(in.readAll());
-            currentFilePath = filePath;
-            setWindowTitle(filePath);
-        }
-    }
-    void saveFile()
-    {
-        if (currentFilePath.isEmpty())
-        {
-            saveFileAs();
-        }
-        else
-        {
-            writeFile(currentFilePath);
+            openFileInTab(filePath);
         }
     }
 
-    void saveAllFiles()
-    {
-        saveFile();
-        QMessageBox::information(this, "Save All", "All files have been saved.");
-    }
-
-    void reloadFile()
-    {
-        if (currentFilePath.isEmpty())
+    void openFileInTab(const QString &filePath) {
+        if (openTabs.contains(filePath)) {
+            tabWidget->setCurrentWidget(openTabs[filePath]);
             return;
-        openFile();
-    }
-
-    void closeFile()
-    {
-        editor->clear();
-        currentFilePath.clear();
-        setWindowTitle("Qt6 Syntax Highlighter");
-    }
-
-    void findText()
-    {
-        QString searchText = QInputDialog::getText(this, "Find", "Enter text to find:");
-        if (!searchText.isEmpty())
-        {
-            editor->find(searchText);
         }
-    }
 
-    void replaceText()
-    {
-        QString searchText = QInputDialog::getText(this, "Replace", "Enter text to find:");
-        if (searchText.isEmpty())
-            return;
-        QString replaceText = QInputDialog::getText(this, "Replace", "Enter replacement text:");
-        editor->setPlainText(editor->toPlainText().replace(searchText, replaceText));
-    }
-
-    void findInFiles()
-    {
-        QMessageBox::information(this, "Find in Files", "Feature not implemented yet.");
-    }
-
-    void replaceInFiles()
-    {
-        QMessageBox::information(this, "Replace in Files", "Feature not implemented yet.");
-    }
-
-    void writeFile(const QString &filePath)
-    {
         QFile file(filePath);
-        if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
-        {
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QMessageBox::warning(this, "Error", "Could not open file:\n" + filePath);
+            return;
+        }
+
+        QTextStream in(&file);
+        QWidget *container = new QWidget();
+        QVBoxLayout *layout = new QVBoxLayout(container);
+        layout->setContentsMargins(0, 0, 0, 0);
+
+        QPlainTextEdit *editor = new QPlainTextEdit(container);
+        editor->setPlainText(in.readAll());
+        editor->setProperty("filePath", filePath);
+        editor->setLineWrapMode(QPlainTextEdit::NoWrap);
+
+        layout->addWidget(editor);
+        file.close();
+
+        int tabIndex = tabWidget->addTab(container, QFileInfo(filePath).fileName());
+        tabWidget->setTabToolTip(tabIndex, filePath);
+        tabWidget->setCurrentIndex(tabIndex);
+
+        openTabs[filePath] = container;
+    }
+
+    void saveCurrentFile() {
+        QWidget *currentTab = tabWidget->currentWidget();
+        if (!currentTab) return;
+
+        QPlainTextEdit *editor = currentTab->findChild<QPlainTextEdit *>();
+        if (!editor) return;
+
+        QString filePath = editor->property("filePath").toString();
+        if (filePath.isEmpty()) return;
+
+        QFile file(filePath);
+        if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
             QMessageBox::warning(this, "Error", "Could not save file");
             return;
         }
@@ -296,14 +212,84 @@ private:
         file.close();
     }
 
-    void saveFileAs()
-    {
+    void closeTab(int index) {
+        QWidget *tab = tabWidget->widget(index);
+        if (!tab) return;
+
+        QString filePath = tab->property("filePath").toString();
+        openTabs.remove(filePath);
+
+        tabWidget->removeTab(index);
+        tab->deleteLater();
     }
 
-    void goToLine()
-    {
+    void goToLine() {
+        QWidget *currentTab = tabWidget->currentWidget();
+        if (!currentTab) return;
+
+        QPlainTextEdit *editor = currentTab->findChild<QPlainTextEdit *>();
+        if (!editor) return;
+
+        bool ok;
+        int lineNumber = QInputDialog::getInt(this, "Go to Line", "Enter line number:", 1, 1, editor->document()->blockCount(), 1, &ok);
+        if (ok) {
+            QTextCursor cursor = editor->textCursor();
+            cursor.movePosition(QTextCursor::Start);
+            cursor.movePosition(QTextCursor::Down, QTextCursor::MoveAnchor, lineNumber - 1);
+            editor->setTextCursor(cursor);
+        }
     }
+
+    // Common Edit Menu actions
+    void selectAll() { getCurrentEditor()->selectAll(); }
+    void copy() { getCurrentEditor()->copy(); }
+    void cut() { getCurrentEditor()->cut(); }
+    void paste() { getCurrentEditor()->paste(); }
+    void undo() { getCurrentEditor()->undo(); }
+    void redo() { getCurrentEditor()->redo(); }
+
+    QPlainTextEdit* getCurrentEditor() {
+        QWidget *currentTab = tabWidget->currentWidget();
+        return currentTab ? currentTab->findChild<QPlainTextEdit *>() : nullptr;
+    }
+ void findText() {
+    QWidget *currentTab = tabWidget->currentWidget();
+    if (!currentTab) return;
+
+    QPlainTextEdit *editor = currentTab->findChild<QPlainTextEdit *>();
+    if (!editor) return;
+
+    bool ok;
+    QString searchText = QInputDialog::getText(this, "Find", "Enter text to find:", QLineEdit::Normal, "", &ok);
+    
+    if (ok && !searchText.isEmpty()) {
+        if (!editor->find(searchText)) {
+            QMessageBox::information(this, "Find", "Text not found.");
+        }
+    }
+}
+
+void replaceText() {
+    QWidget *currentTab = tabWidget->currentWidget();
+    if (!currentTab) return;
+
+    QPlainTextEdit *editor = currentTab->findChild<QPlainTextEdit *>();
+    if (!editor) return;
+
+    bool ok;
+    QString searchText = QInputDialog::getText(this, "Find", "Enter text to find:", QLineEdit::Normal, "", &ok);
+    if (!ok || searchText.isEmpty()) return;
+
+    QString replaceText = QInputDialog::getText(this, "Replace", "Enter replacement text:", QLineEdit::Normal, "", &ok);
+    if (!ok) return;
+
+    QString content = editor->toPlainText();
+    content.replace(searchText, replaceText);
+    editor->setPlainText(content);
+}
+   
 };
+
 
 int main(int argc, char *argv[])
 {
