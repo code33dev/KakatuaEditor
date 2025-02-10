8a16288ef66b67d79024654c1c277630339a6546
code33dev <alberto.huerta.luna@gmail.com>
2025-02-09 18:43:00 -0600

    Implementing menus part 1

--- main.cpp
+++ main.cpp
@@ -10,10 +10,14 @@
 #include <QMenuBar>
 #include <QMenu>
 #include <QAction>
+#include <QMessageBox>
+#include <QInputDialog>
 
-class SyntaxHighlighter : public QSyntaxHighlighter {
+class SyntaxHighlighter : public QSyntaxHighlighter
+{
 public:
-    SyntaxHighlighter(QTextDocument *parent) : QSyntaxHighlighter(parent) {
+    SyntaxHighlighter(QTextDocument *parent) : QSyntaxHighlighter(parent)
+    {
         QTextCharFormat keywordFormat;
         keywordFormat.setForeground(Qt::blue);
         keywordFormat.setFontWeight(QFont::Bold);
@@ -21,8 +25,7 @@ public:
             "\\bint\\b", "\\bdouble\\b", "\\bfloat\\b", "\\bchar\\b",
             "\\breturn\\b", "\\bvoid\\b", "\\bif\\b", "\\belse\\b",
             "\\bfor\\b", "\\bwhile\\b", "\\bclass\\b", "\\bpublic\\b",
-            "\\bprivate\\b", "\\bprotected\\b", "\\bnamespace\\b"
-        };
+            "\\bprivate\\b", "\\bprotected\\b", "\\bnamespace\\b"};
 
         QTextCharFormat commentFormat;
         commentFormat.setForeground(Qt::darkGreen);
@@ -39,10 +42,13 @@ public:
     }
 
 protected:
-    void highlightBlock(const QString &text) override {
-        for (const HighlightingRule &rule : rules) {
+    void highlightBlock(const QString &text) override
+    {
+        for (const HighlightingRule &rule : rules)
+        {
             QRegularExpressionMatchIterator matchIterator = rule.pattern.globalMatch(text);
-            while (matchIterator.hasNext()) {
+            while (matchIterator.hasNext())
+            {
                 QRegularExpressionMatch match = matchIterator.next();
                 setFormat(match.capturedStart(), match.capturedLength(), rule.format);
             }
@@ -50,17 +56,20 @@ protected:
     }
 
 private:
-    struct HighlightingRule {
+    struct HighlightingRule
+    {
         QRegularExpression pattern;
         QTextCharFormat format;
     };
     QVector<HighlightingRule> rules;
 };
 
-class TextEditor : public QMainWindow {
+class TextEditor : public QMainWindow
+{
 public:
-    TextEditor() {
-        setWindowTitle("Qt6 Syntax Highlighter with Menu");
+    TextEditor()
+    {
+        setWindowTitle("Qt6 Syntax Highlighter with Full Menus");
         resize(800, 600);
 
         editor = new QPlainTextEdit(this);
@@ -73,38 +82,156 @@ public:
 private:
     QPlainTextEdit *editor;
     SyntaxHighlighter *highlighter;
+    QString currentFilePath;
 
-    void createMenuBar() {
+    void createMenuBar()
+    {
         QMenuBar *menuBar = new QMenuBar(this);
+
+        // File Menu
         QMenu *fileMenu = menuBar->addMenu("&File");
+        fileMenu->addAction("New", QKeySequence::New, this, &TextEditor::newFile);
+        fileMenu->addAction("Open", QKeySequence::Open, this, &TextEditor::openFile);
+        fileMenu->addAction("Save", QKeySequence::Save, this, &TextEditor::saveFile);
+        fileMenu->addAction("Save All", this, &TextEditor::saveAllFiles);
+        fileMenu->addAction("Reload", this, &TextEditor::reloadFile);
+        fileMenu->addAction("Close File", this, &TextEditor::closeFile);
+        fileMenu->addSeparator();
+        fileMenu->addAction("Exit", QKeySequence::Quit, qApp, &QApplication::quit);
 
-        QAction *openAction = new QAction("Open", this);
-        openAction->setShortcut(QKeySequence::Open);
-        connect(openAction, &QAction::triggered, this, &TextEditor::openFile);
-        fileMenu->addAction(openAction);
-
-        QAction *exitAction = new QAction("Exit", this);
-        exitAction->setShortcut(QKeySequence::Quit);
-        connect(exitAction, &QAction::triggered, qApp, &QApplication::quit);
-        fileMenu->addAction(exitAction);
+        // Edit Menu
+        QMenu *editMenu = menuBar->addMenu("&Edit");
+        editMenu->addAction("Select All", QKeySequence::SelectAll, editor, &QPlainTextEdit::selectAll);
+        editMenu->addAction("Copy", QKeySequence::Copy, editor, &QPlainTextEdit::copy);
+        editMenu->addAction("Cut", QKeySequence::Cut, editor, &QPlainTextEdit::cut);
+        editMenu->addAction("Paste", QKeySequence::Paste, editor, &QPlainTextEdit::paste);
+        editMenu->addSeparator();
+        editMenu->addAction("Undo", QKeySequence::Undo, editor, &QPlainTextEdit::undo);
+        editMenu->addAction("Redo", QKeySequence::Redo, editor, &QPlainTextEdit::redo);
+        editMenu->addSeparator();
+        editMenu->addAction("Find", QKeySequence::Find, this, &TextEditor::findText);
+        editMenu->addAction("Replace", QKeySequence::Replace, this, &TextEditor::replaceText);
+        editMenu->addAction("Find in Files", QKeySequence(Qt::CTRL|Qt::SHIFT|Qt::Key_F), this, &TextEditor::findInFiles);
+        editMenu->addAction("Replace in Files",QKeySequence(Qt::CTRL|Qt::SHIFT|Qt::Key_H) , this, &TextEditor::replaceInFiles);
+        editMenu->addAction("Go to line", QKeySequence(Qt::CTRL | Qt::Key_F12), this, &TextEditor::goToLine);
 
         setMenuBar(menuBar);
     }
 
-    void openFile() {
+    void newFile()
+    {
+        editor->clear();
+        currentFilePath.clear();
+        setWindowTitle("New File - Qt6 Syntax Highlighter");
+    }
+
+    void openFile()
+    {
         QString filePath = QFileDialog::getOpenFileName(this, "Open File", "", "C++ Files (*.cpp *.h);;All Files (*)");
-        if (filePath.isEmpty()) return;
+        if (filePath.isEmpty())
+            return;
 
         QFile file(filePath);
-        if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) return;
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            QMessageBox::warning(this, "Error", "Could not open file");
+            return;
+        }
 
         QTextStream in(&file);
         editor->setPlainText(in.readAll());
         file.close();
+
+        currentFilePath = filePath;
+        setWindowTitle(filePath);
+    }
+
+    void saveFile()
+    {
+        if (currentFilePath.isEmpty())
+        {
+            saveFileAs();
+        }
+        else
+        {
+            writeFile(currentFilePath);
+        }
+    }
+
+    void saveAllFiles()
+    {
+        saveFile();
+        QMessageBox::information(this, "Save All", "All files have been saved.");
+    }
+
+    void reloadFile()
+    {
+        if (currentFilePath.isEmpty())
+            return;
+        openFile();
+    }
+
+    void closeFile()
+    {
+        editor->clear();
+        currentFilePath.clear();
+        setWindowTitle("Qt6 Syntax Highlighter");
+    }
+
+    void findText()
+    {
+        QString searchText = QInputDialog::getText(this, "Find", "Enter text to find:");
+        if (!searchText.isEmpty())
+        {
+            editor->find(searchText);
+        }
+    }
+
+    void replaceText()
+    {
+        QString searchText = QInputDialog::getText(this, "Replace", "Enter text to find:");
+        if (searchText.isEmpty())
+            return;
+        QString replaceText = QInputDialog::getText(this, "Replace", "Enter replacement text:");
+        editor->setPlainText(editor->toPlainText().replace(searchText, replaceText));
+    }
+
+    void findInFiles()
+    {
+        QMessageBox::information(this, "Find in Files", "Feature not implemented yet.");
+    }
+
+    void replaceInFiles()
+    {
+        QMessageBox::information(this, "Replace in Files", "Feature not implemented yet.");
+    }
+
+    void writeFile(const QString &filePath)
+    {
+        QFile file(filePath);
+        if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
+        {
+            QMessageBox::warning(this, "Error", "Could not save file");
+            return;
+        }
+
+        QTextStream out(&file);
+        out << editor->toPlainText();
+        file.close();
+    }
+
+    void saveFileAs()
+    {
+    }
+
+    void goToLine()
+    {
+
     }
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QApplication app(argc, argv);
     TextEditor window;
     window.show();
