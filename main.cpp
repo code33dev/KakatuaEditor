f17acfcb8b398443a528a9eb128f47dcafd69ed8
code33dev <alberto.huerta.luna@gmail.com>
2025-02-10 00:21:55 -0600

    find in files ok!

--- main.cpp
+++ main.cpp
@@ -22,13 +22,196 @@
 #include <QLineEdit>
 #include <QPushButton>
 #include <QCheckBox>
-#include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QTextCursor>
-#include <QMessageBox>
-#include <QRegularExpression>
 #include <QTextDocument>
 #include <QFontDialog>
+#include <QWidget>
+#include <QListWidget>
+#include <QDirIterator>
+
+class FindInFilesWindow : public QWidget
+{
+
+public:
+    explicit FindInFilesWindow(QTabWidget *tabWidget, QMap<QString, QPlainTextEdit *> &openTabs, QWidget *parent = nullptr)
+        : QWidget(parent), tabWidget(tabWidget), openTabs(openTabs)
+    {
+        setWindowTitle("Find in Files");
+        setWindowFlags(Qt::Window | Qt::WindowStaysOnTopHint);
+
+        QLabel *searchLabel = new QLabel("Search for:");
+        searchBox = new QLineEdit(this);
+
+        QLabel *folderLabel = new QLabel("Folder:");
+        folderBox = new QLineEdit(this);
+        QPushButton *browseButton = new QPushButton("Browse", this);
+
+        QLabel *extensionLabel = new QLabel("File Types:");
+        extensionBox = new QLineEdit(this);
+        extensionBox->setPlaceholderText("e.g., *.cpp *.h *.txt");
+
+        caseCheckBox = new QCheckBox("Match Case", this);
+        regexCheckBox = new QCheckBox("Use Regular Expression", this);
+
+        searchButton = new QPushButton("Search", this);
+        resultList = new QListWidget(this);
+
+        QHBoxLayout *folderLayout = new QHBoxLayout();
+        folderLayout->addWidget(folderBox);
+        folderLayout->addWidget(browseButton);
+
+        QVBoxLayout *mainLayout = new QVBoxLayout();
+        mainLayout->addWidget(searchLabel);
+        mainLayout->addWidget(searchBox);
+        mainLayout->addWidget(folderLabel);
+        mainLayout->addLayout(folderLayout);
+        mainLayout->addWidget(extensionLabel);
+        mainLayout->addWidget(extensionBox);
+        mainLayout->addWidget(caseCheckBox);
+        mainLayout->addWidget(regexCheckBox);
+        mainLayout->addWidget(searchButton);
+        mainLayout->addWidget(resultList);
+
+        setLayout(mainLayout);
+
+        connect(browseButton, &QPushButton::clicked, this, &FindInFilesWindow::selectFolder);
+        connect(searchButton, &QPushButton::clicked, this, &FindInFilesWindow::searchFiles);
+        connect(resultList, &QListWidget::itemClicked, this, &FindInFilesWindow::openSelectedFile);
+    }
+
+private slots:
+    void selectFolder()
+    {
+        QString dir = QFileDialog::getExistingDirectory(this, "Select Folder");
+        if (!dir.isEmpty())
+        {
+            folderBox->setText(dir);
+        }
+    }
+
+    void searchFiles()
+    {
+        resultList->clear(); // Clear previous results
+
+        QString searchPattern = searchBox->text();
+        QString folderPath = folderBox->text();
+        QString fileExtensions = extensionBox->text();
+
+        if (searchPattern.isEmpty() || folderPath.isEmpty() || fileExtensions.isEmpty())
+        {
+            QMessageBox::warning(this, "Warning", "Please enter search text, folder, and file extensions.");
+            return;
+        }
+
+        QTextDocument::FindFlags flags;
+        if (caseCheckBox->isChecked())
+            flags |= QTextDocument::FindCaseSensitively;
+
+        QDirIterator it(folderPath, fileExtensions.split(" "), QDir::Files, QDirIterator::Subdirectories);
+        while (it.hasNext())
+        {
+            QString filePath = it.next();
+            QFile file(filePath);
+
+            if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+            {
+                QTextStream in(&file);
+                int lineNumber = 1;
+                while (!in.atEnd())
+                {
+                    QString line = in.readLine();
+                    bool matchFound = false;
+
+                    if (regexCheckBox->isChecked())
+                    {
+                        QRegularExpression regex(searchPattern);
+                        if (!regex.isValid())
+                        {
+                            QMessageBox::warning(this, "Error", "Invalid Regular Expression.");
+                            return;
+                        }
+                        matchFound = line.contains(regex);
+                    }
+                    else
+                    {
+                        matchFound = line.contains(searchPattern, caseCheckBox->isChecked() ? Qt::CaseSensitive : Qt::CaseInsensitive);
+                    }
+
+                    if (matchFound)
+                    {
+                        QListWidgetItem *item = new QListWidgetItem(QString("%1:%2 - %3").arg(filePath).arg(lineNumber).arg(line));
+                        item->setData(Qt::UserRole, filePath);       // Store file path
+                        item->setData(Qt::UserRole + 1, lineNumber); // Store line number
+                        resultList->addItem(item);
+                    }
+
+                    lineNumber++;
+                }
+                file.close();
+            }
+        }
+
+        if (resultList->count() == 0)
+        {
+            QMessageBox::information(this, "Find in Files", "No matches found.");
+        }
+    }
+
+    void openSelectedFile(QListWidgetItem *item)
+    {
+        QString filePath = item->data(Qt::UserRole).toString();
+        int lineNumber = item->data(Qt::UserRole + 1).toInt();
+
+        if (!filePath.isEmpty())
+        {
+            openFileAtLine(filePath, lineNumber);
+        }
+    }
+
+private:
+    QLineEdit *searchBox, *folderBox, *extensionBox;
+    QPushButton *searchButton;
+    QCheckBox *caseCheckBox, *regexCheckBox;
+    QListWidget *resultList;
+    QTabWidget *tabWidget;
+    QMap<QString, QPlainTextEdit *> &openTabs;
+
+    void openFileAtLine(const QString &filePath, int lineNumber)
+    {
+        if (openTabs.contains(filePath))
+        {
+            tabWidget->setCurrentWidget(openTabs[filePath]); // If file is already open, switch to it
+        }
+        else
+        {
+            QFile file(filePath);
+            if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+            {
+                QTextStream in(&file);
+                QPlainTextEdit *editor = new QPlainTextEdit();
+                editor->setPlainText(in.readAll());
+                editor->setProperty("filePath", filePath);
+                editor->setLineWrapMode(QPlainTextEdit::NoWrap);
+
+                int tabIndex = tabWidget->addTab(editor, QFileInfo(filePath).fileName());
+                tabWidget->setTabToolTip(tabIndex, filePath);
+                tabWidget->setCurrentIndex(tabIndex);
+
+                openTabs[filePath] = editor;
+                file.close();
+            }
+        }
+
+        // Move cursor to the specific line
+        QPlainTextEdit *editor = openTabs[filePath];
+        QTextCursor cursor = editor->textCursor();
+        cursor.movePosition(QTextCursor::Start);
+        cursor.movePosition(QTextCursor::Down, QTextCursor::MoveAnchor, lineNumber - 1);
+        editor->setTextCursor(cursor);
+        editor->setFocus();
+    }
+};
 
 class ReplaceWindow : public QWidget
 {
@@ -400,7 +583,12 @@ private:
     QFileSystemModel *fileModel;
     QTabWidget *tabWidget;
     QMap<QString, QPlainTextEdit *> openTabs;
-
+    
+    void findInFilesWithWindow()
+    {
+        FindInFilesWindow *findWindow = new FindInFilesWindow(tabWidget, openTabs);
+        findWindow->show();
+    }
     void replaceWithWindow()
     {
         QPlainTextEdit *editor = getCurrentEditor();
@@ -447,7 +635,7 @@ private:
         editMenu->addAction("Find", QKeySequence::Find, this, &TextEditor::findWithWindow);
         editMenu->addAction("Replace", QKeySequence::Replace, this, &TextEditor::replaceWithWindow);
         editMenu->addSeparator();
-        editMenu->addAction("Find in Files", QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_F), this, &TextEditor::findInFiles);
+        editMenu->addAction("Find in Files", QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_F), this, &TextEditor::findInFilesWithWindow);
         editMenu->addAction("Replace in Files", QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_H), this, &TextEditor::replaceInFiles);
         editMenu->addSeparator();
         editMenu->addAction("Go to Line", QKeySequence(Qt::CTRL | Qt::Key_F12), this, &TextEditor::goToLine);
