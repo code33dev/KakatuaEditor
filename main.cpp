3d17db349e9fbb707c9547b8c630600934639ad1
code33dev <alberto.huerta.luna@gmail.com>
2025-02-09 18:27:08 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,13 @@
 #include <QSyntaxHighlighter>
 #include <QRegularExpression>
 #include <QTextCharFormat>
+#include <QMenuBar>
+#include <QMenu>
+#include <QAction>
 
 class SyntaxHighlighter : public QSyntaxHighlighter {
 public:
     SyntaxHighlighter(QTextDocument *parent) : QSyntaxHighlighter(parent) {
-        // Define keyword format
         QTextCharFormat keywordFormat;
         keywordFormat.setForeground(Qt::blue);
         keywordFormat.setFontWeight(QFont::Bold);
@@ -22,21 +24,18 @@ public:
             "\\bprivate\\b", "\\bprotected\\b", "\\bnamespace\\b"
         };
 
-        // Define comment format
         QTextCharFormat commentFormat;
         commentFormat.setForeground(Qt::darkGreen);
         commentFormat.setFontItalic(true);
 
-        // Define string format
         QTextCharFormat stringFormat;
         stringFormat.setForeground(Qt::darkRed);
 
-        // Store patterns and formats
         for (const QString &pattern : keywordPatterns)
             rules.append({QRegularExpression(pattern), keywordFormat});
 
-        rules.append({QRegularExpression("//[^\n]*"), commentFormat});  // Single-line comments
-        rules.append({QRegularExpression("\".*?\""), stringFormat});    // Strings
+        rules.append({QRegularExpression("//[^\n]*"), commentFormat});
+        rules.append({QRegularExpression("\".*?\""), stringFormat});
     }
 
 protected:
@@ -61,19 +60,36 @@ private:
 class TextEditor : public QMainWindow {
 public:
     TextEditor() {
-        setWindowTitle("Qt6 Syntax Highlighting");
+        setWindowTitle("Qt6 Syntax Highlighter with Menu");
         resize(800, 600);
 
         editor = new QPlainTextEdit(this);
         setCentralWidget(editor);
         highlighter = new SyntaxHighlighter(editor->document());
 
-        openFile();
+        createMenuBar();
     }
 
 private:
     QPlainTextEdit *editor;
     SyntaxHighlighter *highlighter;
+
+    void createMenuBar() {
+        QMenuBar *menuBar = new QMenuBar(this);
+        QMenu *fileMenu = menuBar->addMenu("&File");
+
+        QAction *openAction = new QAction("Open", this);
+        openAction->setShortcut(QKeySequence::Open);
+        connect(openAction, &QAction::triggered, this, &TextEditor::openFile);
+        fileMenu->addAction(openAction);
+
+        QAction *exitAction = new QAction("Exit", this);
+        exitAction->setShortcut(QKeySequence::Quit);
+        connect(exitAction, &QAction::triggered, qApp, &QApplication::quit);
+        fileMenu->addAction(exitAction);
+
+        setMenuBar(menuBar);
+    }
 
     void openFile() {
         QString filePath = QFileDialog::getOpenFileName(this, "Open File", "", "C++ Files (*.cpp *.h);;All Files (*)");
